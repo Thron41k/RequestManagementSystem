07b8459bb59102d9d7d7cb214e6e3479832846af
Timothy <Thron41k@gmail.com>
2025-08-22 13:06:51 +0900

    update

--- RequestManagement.Server/Services/IncomingService.cs
+++ RequestManagement.Server/Services/IncomingService.cs
@@ -65,8 +65,8 @@ public class IncomingService(ApplicationDbContext dbContext) : IIncomingService
                 query = query.Where(e => e.Date < toDate.AddDays(1));
             }
         }
-        Console.WriteLine($"FROM: {fromDate:o}, TO: {toDate:o}");
-        Console.WriteLine(query.ToQueryString());
+        Debug.WriteLine($"FROM: {fromDate:o}, TO: {toDate:o}");
+        Debug.WriteLine(query.ToQueryString());
         return await query.ToListAsync();
     }
 
