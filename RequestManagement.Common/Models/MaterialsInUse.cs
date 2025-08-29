6beaf748c8fec20d942529784cf484ca7916c14e
Timothy <Thron41k@gmail.com>
2025-08-29 22:29:57 +0900

    update

--- RequestManagement.Common/Models/MaterialsInUse.cs
+++ RequestManagement.Common/Models/MaterialsInUse.cs
@@ -28,5 +28,5 @@ public class MaterialsInUse : IEntity
     public int? ExpenseId { get; set; }
     public int Term { get; set; }
     [NotMapped] public string DateForList => DateForWriteOff == DateTime.MinValue ? "" : DateForWriteOff.ToString("dd.MM.yyyy");
-    [NotMapped] public string ServiceLife => ((DateTime.Now.Year - Date.Year) * 12 + DateTime.Now.Month - Date.Month - 1).ToString();
+    [NotMapped] public string ServiceLife => ((DateTime.Now.Year - Date.Year) * 12 + DateTime.Now.Month - Date.Month).ToString();
 }
